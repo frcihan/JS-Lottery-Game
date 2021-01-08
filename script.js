4e9f66a95d1a9022eeb02ce32380b2adb5c5b89a
ferruhcihan <ferruhcihan20@gmail.com>
2021-01-08 20:00:04 +0300

    script.js updated

--- script.js
+++ script.js
@@ -6,8 +6,22 @@ function shuffle(array) {
 }
 
 numbers = Array.from({length: 49}, (_, i) => i + 1);
-console.log(numbers);
+console.log("initial numbers:", numbers);
 
 lottery_button.addEventListener("click", () => {
+    shuffle(numbers);
+    console.log("shuffled numbers:", numbers);
+    
+    selected_numbers = numbers.slice(0,6);
+    console.log("selected numbers:", selected_numbers);
+
+    sorted_numbers = selected_numbers.sort(function(a, b){return a - b})
+    console.log("sorted_numbers:", sorted_numbers);
+
+    let i = 0;
+    buttons.forEach((btn) => {
+        btn.textContent = sorted_numbers[i];
+        i++;
+    })
 
 })
